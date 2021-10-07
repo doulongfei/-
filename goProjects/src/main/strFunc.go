adfbf8adfd9702ade14e9db707980ac9e1ebdc69
doulongfei <2648142550@qq.com>
2021-10-07 21:29:00 +0800

    zifuchuan

---
+++ goProjects/src/main/strFunc.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+
+	"fmt"
+	"strings"
+	"strconv"
+)
+func main() {
+	var str string ="hello"
+
+	fmt.Println(len(str))
+
+	for i, v := range str {
+		fmt.Println("索引：%d,值：%d",i,v)
+	}
+
+	for _,value:=range str{
+		fmt.Println("值：%d",value)
+	}
+
+
+	// 字符串转整数
+	n,err:=strconv.Atoi("66")
+	fmt.Println(n,err)
+
+	str1:=strconv.Itoa(23434)
+	fmt.Println(str1)
+
+	// 统计一个字符串中有几个指定的字串
+
+	count:=strings.Count("golangandjavaga","ga")
+
+	fmt.Println(count)
+
+	// 不区分大小写的字符串比较
+	flag:=strings.EqualFold("hello","HELLO")
+
+	fmt.Println(flag)
+
+	// 区分大小写的字符串比较
+
+	fmt.Println("hello"=="HELLo")
+	
+
+}
